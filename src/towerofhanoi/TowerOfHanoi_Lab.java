a1506af840a7ec7e4bbce264df5c41811d2a0b42
ntck <nontakorn.chatk@gmail.com>
2023-08-31 19:17:24 +0700

    First commit

---
+++ src/towerofhanoi/TowerOfHanoi_Lab.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package towerofhanoi;
+
+/**
+ *
+ * @author ntck
+ */
+public class TowerOfHanoi_Lab {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TowerOfHanoi t1 = new TowerOfHanoi(3);
+        t1.play();
+    }
+    
+}
